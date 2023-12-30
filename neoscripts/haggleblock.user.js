cd0ffaae3e217b9d3c67db79b4af6a1ca57990f0
Jon <Xiono.Helfeather@gmail.com>
2023-12-29 23:47:23 -0500

    Update haggleblock.user.js

--- neoscripts/haggleblock.user.js
+++ neoscripts/haggleblock.user.js
@@ -1,1 +1,20 @@
-// ==UserScript== // @name         Block Haggle Auto Complete // @version      1.0 // @description  Block haggle auto complete // @match        https://www.neopets.com/haggle.phtml* // @match        https://www.neopets.com/haggle.phtml // @grant        none // ==/UserScript==  (function() {     'use strict';      function disableAutocomplete() {         var inputs = document.getElementsByTagName('input');         for (var i = 0; i < inputs.length; i++) {             inputs[i].setAttribute('autocomplete', 'off');         }     }     window.addEventListener('load', disableAutocomplete); })();
+// ==UserScript==
+// @name         Block Haggle Auto Complete
+// @version      1.0
+// @description  Block haggle auto complete
+// @match        https://www.neopets.com/haggle.phtml*
+// @match        https://www.neopets.com/haggle.phtml
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    function disableAutocomplete() {
+        var inputs = document.getElementsByTagName('input');
+        for (var i = 0; i < inputs.length; i++) {
+            inputs[i].setAttribute('autocomplete', 'off');
+        }
+    }
+    window.addEventListener('load', disableAutocomplete);
+})();
